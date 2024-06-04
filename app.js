54f23b3d163e123db5f1208593d0730db091f58f
Asad-Nawaz92 <asadn7862@gmail.com>
2024-06-04 16:41:13 +0500

    Improve the Performance

--- app.js
+++ app.js
@@ -4,50 +4,25 @@ const firebaseConfig = {
   projectId: "signup-auth-474ba",
   storageBucket: "signup-auth-474ba.appspot.com",
   messagingSenderId: "132636142418",
-  appId: "1:132636142418:web:91edf99f16e2e048d4a88b"
+  appId: "1:132636142418:web:91edf99f16e2e048d4a88b",
 };
 
+// Initialize Firebase
 const fireBase = firebase.initializeApp(firebaseConfig);
 
+// SignUp/SignIn Form
 function signup() {
-  var emailInput = document.getElementById("signup-email");
-  var passwordInput = document.getElementById("signup-pass");
+  const emailInput = document.getElementById("signup-email");
+  const passwordInput = document.getElementById("signup-pass");
 
-  var email = emailInput.value;
-  var password = passwordInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-  if (email === "" || password === "") {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Please fill in all fields.",
-    });
-    return;
-  }
-
-  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-  if (!emailPattern.test(email)) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Invalid email format. Please use the format: example@gmail.com.",
-    });
-    return;
-  }
-
-  if (password.length < 6) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Password must be at least 6 characters long.",
-    });
-    return;
-  }
-
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-   .then((userCredential) => {
-      var user = userCredential.user;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
       Swal.fire({
         icon: "success",
         title: "Success",
@@ -56,9 +31,9 @@ function signup() {
         window.location.href = "./signin.html";
       });
     })
-   .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
       Swal.fire({
         icon: "error",
         title: "Oops...",
@@ -68,40 +43,22 @@ function signup() {
 }
 
 function signin() {
-  var emailInput = document.getElementById("signin-email");
-  var passwordInput = document.getElementById("signin-pass");
+  const emailInput = document.getElementById("signin-email");
+  const passwordInput = document.getElementById("signin-pass");
 
-  var email = emailInput.value;
-  var password = passwordInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-  if (email === "" || password === "") {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Please fill in all fields.",
-    });
-    return;
-  }
-
-  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-  if (!emailPattern.test(email)) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Invalid email format. Please use the format: example@gmail.com.",
-    });
-    return;
-  }
-
-  firebase.auth().signInWithEmailAndPassword(email, password)
-   .then((userCredential) => {
-      var user = userCredential.user;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
       window.location.href = "./welcome.html";
     })
-   .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
       Swal.fire({
         icon: "error",
         title: "Oops...",
@@ -110,6 +67,6 @@ function signin() {
     });
 }
 
-function logOut(){
+function logOut() {
   window.location.href = "./signin.html";
 }
